bfd4c102cb9cc6cf7f0582f642f5d826b0983da4
Your Name <you@example.com>
2026-07-02 15:17:01 +0600

    first push

---
+++ app/(modules)/home/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import MainHome from "./page/MainHome";
+
+const HomePage: React.FC = () => {
+  return <MainHome />;
+};
+export default HomePage;
+
---
+++ app/(modules)/home/page/MainHome.tsx
@@ -0,0 +1,26 @@
+"use Client";
+import { Slider1, Slider2, Slider3, Slider15, Slider16 } from "@/assets";
+import Carousel from "@/components/Carousel";
+import { SlideProps } from "@/types/CommonProps";
+import RecentNewsSection from "./section/RecentNewsSection";
+import OurStorySection from "./section/OurStorySection";
+
+const MainHome: React.FC = () => {
+  const slides: SlideProps[] = [
+    { id: 1, img: Slider1, title: "Beautiful Landscape" },
+    { id: 2, img: Slider2, title: "City Night Lights" },
+    { id: 3, img: Slider3, title: "Mountain Adventure" },
+    { id: 4, img: Slider15, title: "Mountain Adventure" },
+    { id: 5, img: Slider16, title: "Mountain Adventure" },
+  ];
+  return (
+    <div>
+      <section>
+        <Carousel slides={slides} />
+      </section>
+      <RecentNewsSection />
+      <OurStorySection />
+    </div>
+  );
+};
+export default MainHome;
